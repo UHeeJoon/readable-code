5d6e84160036b989db72309b987a56585cfc1a8c
Uheejoon <yhjyhj3855@gmail.com>
2024-10-30 16:51:47 +0900

    ISP 적용

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,14 +1,16 @@
 package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel level = new VeryBeginner();
+        GameLevel level = new Beginner();
         Minesweeper minesweeper = new Minesweeper(level);
+        minesweeper.initialize();
         minesweeper.run();
     }
 
