2c13fc8570c5def18abafedd7efaea77b150908e
Uheejoon <yhjyhj3855@gmail.com>
2024-10-30 17:49:25 +0900

    DIP 적용

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -4,14 +4,32 @@ import cleancode.minesweeper.tobe.gamelevel.Advanced;
 import cleancode.minesweeper.tobe.gamelevel.Beginner;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
         GameLevel level = new Beginner();
-        Minesweeper minesweeper = new Minesweeper(level);
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+
+
+        Minesweeper minesweeper = new Minesweeper(level, inputHandler, outputHandler);
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /**
+     * DIP (Dependency Inversion Principle)
+     *
+     * Spring Di (Dependency Injection) - 제 3자 IoC컨테이너가 주입
+     * 필요한 의존성을 외부에서 주입
+     *
+     * IoC (Inversion of Control)
+     * 프로그램의 흐름을 개발자가 아닌 프레임워크가 제어
+     */
 
 }
